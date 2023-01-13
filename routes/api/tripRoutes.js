71e299787741decc89fa90f67e033b16cc4e0ef4
kassrojas <kassandrarojass@gmail.com>
2023-01-13 14:45:55 -0500

    finsihed product

---
+++ routes/api/tripRoutes.js
@@ -0,0 +1,25 @@
+const router = require('express').Router();
+const { Trip } = require('../../models');
+
+router.post('/', async (req, res) => {
+    try {
+        const tripData = await Trip.create(req.body);
+        res.json(tripData);
+    } catch (err) {
+        console.error(err);
+        res.json(err);
+    }
+});
+
+router.delete('/:id', async (req, res) => {
+    try {
+        const tripData = await Trip.destroy({ where: { id: req.params.id } });
+        res.json(tripData);
+    } catch (err) {
+        console.error(err);
+        res.json(err);
+    }
+});
+
+
+module.exports = router;
